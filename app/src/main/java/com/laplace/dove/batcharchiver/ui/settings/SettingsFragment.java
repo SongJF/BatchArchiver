f7df6253207cbc4eab3d07773b729440a4f89fa6
SongJF <LaplaceOperate@outlook.com>
2021-12-29 23:20:40 +0800

    init

---
+++ app/src/main/java/com/laplace/dove/batcharchiver/ui/settings/SettingsFragment.java
@@ -0,0 +1,46 @@
+package com.laplace.dove.batcharchiver.ui.settings;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.laplace.dove.batcharchiver.R;
+import com.laplace.dove.batcharchiver.databinding.FragmentSettingsBinding;
+
+public class SettingsFragment extends Fragment {
+
+    private SettingsViewModel settingsViewModel;
+    private FragmentSettingsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        settingsViewModel =
+                new ViewModelProvider(this).get(SettingsViewModel.class);
+
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textSettings;
+        settingsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
